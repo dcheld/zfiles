ae9e57af21854d25e8ba7c17d3256285afaf6c3d
Douglas Held <dcheld@gmail.com>
2022-10-29 11:38:41 -0300

    Ignore duplicate commands and remove invalid commands from history

--- history.zsh
+++ history.zsh
@@ -3,9 +3,13 @@
 export SAVEHIST=1000
 export HISTSIZE=1000
 export HISTFILE="$HOME/.zsh_history"
+
+# Not store failed command
+zshaddhistory() { whence ${${(z)1}[1]} >| /dev/null || return 1 }
+
 # setopt extended_history       # record timestamp of command in HISTFILE
 # setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-# setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_dups       # ignore duplicated commands history list
 # setopt hist_ignore_space      # ignore commands that start with space
 # setopt hist_verify            # show command with history expansion to user before running it
 # setopt share_history          # share command history data
