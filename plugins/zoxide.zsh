aa0680f76d7416dbff2fb3cedb856875886da2f9
Douglas Held <dcheld@gmail.com>
2024-10-18 10:21:36 -0300

    Change way to source zoxide init

--- plugins/zoxide.zsh
+++ plugins/zoxide.zsh
@@ -2,9 +2,12 @@
 
 ## Interactive command depends of fzf.zsh
 zinit light-mode from"gh-r" as"null" completions for \
-        atclone"cp -f man/man1/* $ZPFX/man/man1/;" \
+        atclone" \
+            cp -f man/man1/* $ZPFX/man/man1/; \
+            ./zoxide init zsh --cmd cd > zoxide_init.zsh; \
+        " \
         atpull'%atclone' \
         sbin"zoxide" \
-        atload'eval "$(zoxide init zsh --cmd cd)"' \
+        src"zoxide_init.zsh" \
         id-as"zoxide" \
     @ajeetdsouza/zoxide
