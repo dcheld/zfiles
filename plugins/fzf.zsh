533190a0d79b8b32ae936aacd6cb364a4c610721
Douglas Held <dcheld@gmail.com>
2024-10-19 12:07:54 -0300

    Install fzf using release of github

--- plugins/fzf.zsh
+++ plugins/fzf.zsh
@@ -1,3 +1,12 @@
 #!/usr/bin/env zsh
 
-zinit pack for fzf
+zinit light-mode wait lucid nocompletions for \
+        from"gh-r" \
+        sbin"fzf" \
+        id-as"fzf" \
+    junegunn/fzf \
+        has"tmux" \
+        as"null" \
+        sbin"fzf-tmux" \
+        id-as"fzf-tmux" \
+    "https://raw.githubusercontent.com/junegunn/fzf/refs/heads/master/bin/fzf-tmux"
