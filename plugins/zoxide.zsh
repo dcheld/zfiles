21e5e5ad7b09c243cff1a38e0670c0950762f4d0
Douglas Held <douglas.held@ciandt.com>
2024-11-02 10:48:51 -0300

    Remove source, when scripts init.zsh is not necessary source

--- plugins/zoxide.zsh
+++ plugins/zoxide.zsh
@@ -1,13 +1,12 @@
 #!/usr/bin/env zsh
 
 ## Interactive command depends of fzf.zsh
-zinit light-mode from"gh-r" as"command" completions for \
-        atclone" \
-            cp -f man/man1/* $ZPFX/man/man1/; \
-            ./zoxide init zsh --cmd cd > zoxide_init.zsh; \
+zinit light-mode from"gh-r" completions for \
+        atclone"
+            cp -f man/man1/* $ZPFX/man/man1/;
+            ./zoxide init zsh --cmd cd > init.zsh;
         " \
         atpull'%atclone' \
         lbin"zoxide" \
-        src"zoxide_init.zsh" \
         id-as"zoxide" \
     @ajeetdsouza/zoxide
