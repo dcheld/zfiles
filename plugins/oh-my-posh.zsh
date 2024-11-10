a9761322b90335fa1aca02190ad536599a629f50
Douglas Held <douglas.held@ciandt.com>
2024-11-10 11:58:53 -0300

    Export environment variable oh my posh theme

--- plugins/oh-my-posh.zsh
+++ plugins/oh-my-posh.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
-local CONFIG="${1:-jandedobbeleer}"
+export OH_MY_POSH_THEME="${1:-jandedobbeleer}"
 zinit light-mode lucid from"gh-r" for \
         mv"posh*->oh-my-posh" \
         lbin"oh-my-posh" \
         atclone'
-            local OMP_TEMPLATE_URL="https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/${CONFIG}.omp.json"
+            local OMP_TEMPLATE_URL="https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/${OH_MY_POSH_THEME}.omp.json"
             ./oh-my-posh completion zsh > _oh-my-posh;
             ./oh-my-posh init zsh --config "$OMP_TEMPLATE_URL" > init.zsh;' \
         atpull"%atclone" \
