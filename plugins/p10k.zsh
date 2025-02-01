1d5fd3239503f0700bf81a9f2ada00cd3eaa0076
Douglas Held <dcheld@gmail.com>
2025-02-01 15:16:24 -0300

    Add instant prompt in p10k

--- plugins/p10k.zsh
+++ plugins/p10k.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 #PowerLevel10K
 zinit light-mode depth=1 for \
         atload'[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' \
