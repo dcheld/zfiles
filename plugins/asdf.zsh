7a0e005e5579a48c5e30717bbe1c86485d67c82a
Douglas Held <dcheld@gmail.com>
2025-02-01 15:14:52 -0300

    Migrate to version 0.16 (rewritten in go)

--- plugins/asdf.zsh
+++ plugins/asdf.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
-zinit light-mode depth'1' lucid for \
-        wait'1' \
+zinit light-mode lucid wait from"gh-r" for \
+        lbin"asdf" \
+        atclone"
+            ./asdf completion zsh > _asdf" \
+        atpull"%atclone" \
     @asdf-vm/asdf
