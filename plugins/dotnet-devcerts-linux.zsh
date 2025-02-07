6eb9e784af295fa2443520edf8ea151caca8a15f
Douglas Held <douglas.held@ciandt.com>
2025-02-07 19:06:40 -0300

    Fix wrong reference in dotnet devcerts

--- plugins/dotnet-devcerts-linux.zsh
+++ plugins/dotnet-devcerts-linux.zsh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 
 zinit light-mode wait lucid for \
-        sbin"scripts/common.sh -> dotnet-devcerts-linux" \
+        atclone$'
+            local NAME=$(awk -F= \'$1=="ID" { print $2 ;}\' /etc/os-release | tr "[:upper:]" "[:lower:]")
+            cp "scripts/$NAME-create-dotnet-devcert.sh" scripts/dotnet-devcerts-linux' \
+        atpull'%atclone' \
+        sbin'scripts/dotnet-devcerts-linux' \
         nocompile \
         id-as"dotnet-devcerts-linux" \
     @BorisWilhelms/create-dotnet-devcert
