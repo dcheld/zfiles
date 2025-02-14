05eccea014cab65343ffda6287116f70b1d0dbe7
Douglas Held <dcheld@gmail.com>
2025-02-13 21:51:23 -0300

    Change dotnet-install to copy file, avoid git abort

--- plugins/dotnet.zsh
+++ plugins/dotnet.zsh
@@ -4,7 +4,8 @@
 zinit light-mode depth'1' lucid for \
         wait \
         as"null" \
-        lbin"src/dotnet-install.sh -> dotnet-install" \
+        cp"src/dotnet-install.sh -> dotnet-install" \
+        lbin"dotnet-install" \
     dotnet/install-scripts \
         wait \
         if'[[ -f "$HOME/.dotnet/dotnet" ]]' \
