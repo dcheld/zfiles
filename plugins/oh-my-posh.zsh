9e938d65153f29d230309534b08354afe00c6038
Douglas Held <dcheld@gmail.com>
2025-08-17 14:02:22 -0300

    Adjust oh-my-posh to load automatic theme

--- plugins/oh-my-posh.zsh
+++ plugins/oh-my-posh.zsh
@@ -4,8 +4,13 @@ export OH_MY_POSH_THEME="${1:-jandedobbeleer}"
 zinit light-mode lucid from"gh-r" for \
         mv"posh*->oh-my-posh" \
         lbin"oh-my-posh" \
-        atclone'
-            ./oh-my-posh init zsh --config "$OH_MY_POSH_THEME" > init.zsh;' \
+        atclone'mkdir -p themes' \
         atpull"%atclone" \
+        atinit"
+            if [[ ! -f themes/${OH_MY_POSH_THEME} ]]; then
+                rm -rf themes/* > /dev/null 2>&1;
+                oh-my-posh init zsh --config ${OH_MY_POSH_THEME} > init.zsh \
+                    && touch themes/${OH_MY_POSH_THEME};
+            fi" \
         id-as"oh-my-posh" \
     JanDeDobbeleer/oh-my-posh
--- plugins/starship.zsh
+++ plugins/starship.zsh
@@ -4,11 +4,14 @@ local theme=${1:-gruvbox-rainbow}
 zinit light-mode lucid completions from"gh-r" for \
         atclone"
             ./starship init zsh > init.zsh;
-            ./starship completions zsh > _starship
-            if [[ -n "$theme" ]]; then
-                ./starship preset "$theme" -o $HOME/.config/starship.toml
-            fi" \
+            ./starship completions zsh > _starship;
+            mkdir -p themes;" \
         atpull"%atclone" \
+        atinit"
+            if [[ ! -f themes/${theme} ]]; then
+                rm -rf themes/* > /dev/null 2>&1;
+                ./starship preset ${theme} -o $HOME/.config/starship.toml \
+                    && touch themes/${theme};
+            fi" \
         lbin'starship' \
-        src"init.zsh" \
     @starship/starship
