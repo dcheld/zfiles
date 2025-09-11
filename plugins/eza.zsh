3210542de4537e858958e4b5856d44a0a861c94c
Douglas Held <dcheld@gmail.com>
2025-09-10 23:31:46 -0300

    Download manual and completions

--- plugins/eza.zsh
+++ plugins/eza.zsh
@@ -1,11 +1,16 @@
 #!/usr/bin/env zsh
 
-zinit light-mode lucid from"gh-r" completions for \
-        wait \
+zinit light-mode lucid from"gh-r" wait for \
+        bpick"eza*;man-*;completions-*;" \
+        atclone'rm target/completions-*/eza' \
+        atpull'%atclone' \
+        mv"
+            target/man*/eza.1 -> $ZPFX/man/man1/;
+            target/man*/eza*.5 -> $ZPFX/man/man5/;" \
+        completions'target/completions-*/' \
+        lbin'eza' \
         atload"alias ls='eza --color=always --icons=auto';
                alias ll='eza --color=always --icons=auto -lagF';
                alias la='eza --color=always --icons=auto -lag';" \
-        lbin"eza" \
-        dl"https://raw.githubusercontent.com/eza-community/eza/main/completions/zsh/_eza -> _eza" \
-        id-as"eza" \
+        id-as'eza' \
     eza-community/eza
