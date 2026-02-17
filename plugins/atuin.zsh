abc51a333173825caa3277ae91085d183490bd57
Douglas Held <dcheld@gmail.com>
2026-02-17 19:40:06 -0300

    Adjust atuin scripts

--- plugins/atuin.zsh
+++ plugins/atuin.zsh
@@ -2,7 +2,7 @@
 
 zinit light-mode lucid for wait \
         from"gh-r" \
-        bpick"atuin-*.tar.gz" \
+        bpick"atuin-${MACHTYPE}*.tar.gz" \
         mv"atuin*/atuin -> atuin" \
         atclone"
             ./atuin init zsh --disable-up-arrow > init.zsh; 
